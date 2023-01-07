bcbef5c0ce0fb30b42e73bc92e05cf7ba82766be
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-01-07 12:52:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,47 @@ let arr = [
 ];
 
 function PrintDeveloperswithMap() {
-  //Write your code here
+  arr.map(printThroghMap);
+  
+  function printThroghMap(arrayItem) {
+    if (arrayItem.profession ==="developer") {
+      console.log(arrayItem);
+    }
+  }
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here
+  arr.forEach(printThroghForEach);
+
+  function printThroghForEach(arrayItem) {
+    if (arrayItem.profession === "developer") {
+      console.log(arrayItem);
+    }
+  }
 }
 
 function addData() {
-  //Write your code here
+ let newData = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newData);
+  console.log(arr);
 }
 
 function removeAdmin() {
-  //Write your code here
+  let remove_arr = arr.filter(function (val) {
+    if (val.profession !== "admin") {
+      return val;
+    }
+  });
+  console.log(remove_arr);
 }
 
 function concatenateArray() {
-  //Write your code here
+   let arr2 = [
+    { id: 5, name: "avinahK", age: "24", profession: "frontend developer" },
+    { id: 6, name: "amanS", age: "25", profession: "backend developer" },
+    { id: 7, name: "vishalC", age: "27", profession: "project engineer" },
+  ];
+
+  let concatArray = arr.concat(arr2);
+  console.log(concatArray);
 }
